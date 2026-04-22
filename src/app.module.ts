adc3d79b42ace53faad4b822f375f21c241e1ba2
Khaiam Garaevv <garaevvkhaiam@gmail.com>
2026-04-22 15:25:09 +0400

    created: quiz, users folders, and any more

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,13 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
+import { QuizController } from './quiz/quiz.controller';
+import { QuizService } from './quiz/quiz.service';
+import { QuizModule } from './quiz/quiz.module';
 
 @Module({
-  imports: [UsersModule],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [UsersModule, QuizModule],
+  controllers: [AppController, QuizController],
+  providers: [AppService, QuizService],
 })
 export class AppModule {}
